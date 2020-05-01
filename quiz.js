fd170ef040c77b7699d032b4d2f371dfc5d9bacc
heather swan <heatherswan@heathers-MacBook-Air.local>
2020-05-01 16:40:54 -0700

    adding firtst files

---
+++ quiz.js
@@ -0,0 +1,70 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+var secondsLeft = 60;
+
+// the song is 33 seconds long
+var themeSong = "assets/jeopardy.mp3";
+var failSong = "assets/boo.wav"
+var successSong = "assets/applause.wav";
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
+
+// question and answer area
+
+
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = " ";
+
+  var imgEl = document.createElement("img");
+
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
